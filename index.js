b26c61e15ede44bee5797ee429ea340245b0d264
Tim254 <tim.mailu@gmail.com>
2022-06-02 20:59:08 +0300

    commit tree f086ca2b336e863f6a7691e13428ac5250697028

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const starGift = [];
+const starWarriors = ["Boba", "CadBane", "Mandalorian"];
+
+function writeCards(starWarriors, Victory) {
+    for (let i = 0; i < starWarriors.length; i++) {
+        starGift.push(`Thank you, ${starWarriors[i]}, for the wonderful ${Victory} gift!`);
+    }
+    return starGift;
+}
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    }
+}
